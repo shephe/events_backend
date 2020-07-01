08b8ec0f1be06b62a6a0c40201865d0b2cdb0e22
Sheila Kelley <spkelley26@gmail.com>
2020-06-30 17:57:35 -0700

    added address to model

--- models/events.js
+++ models/events.js
@@ -2,6 +2,7 @@ const {Schema, model} = require('mongoose')
 
 const eventSchema = new Schema({
     location: {type: String, required: true},
+    address: String,
     startTime: {type: Date, required: true},
     endTime: Date,
     occasion: String,
